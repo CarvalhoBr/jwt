4b26550aae71aa4a8140250b5b00d6b89b526713
CarvalhoBr <brandonsouza27@gmail.com>
2020-06-17 12:56:18 -0300

    Sistema de cadastro criado

---
+++ src/controllers/authController.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const User = require('../models/user')
+
+const router = express.Router()
+
+router.post('/register', async(req, res) => {
+    
+    const {email} = req.body
+    
+    try {
+        if( await User.findOne({ email })){
+            return res.status(400).send({error: 'Ueser already exists'})
+        }
+
+        const user = await User.create(req.body)
+
+        user.password = undefined
+
+        return res.send(user)
+    } catch (error) {
+        res.status(400).send({error: error.stack})
+    }
+})
+
+module.exports = app => app.use('/auth', router)
