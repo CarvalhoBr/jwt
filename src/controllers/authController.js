71d9a903dff06509d4f4cc12315de72f38d6ef1e
CarvalhoBr <brandonsouza27@gmail.com>
2020-06-25 15:43:51 -0300

    Adicionada geração e validação de token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,27 +2,40 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+const authConfig = require('../config/auth.json')
+
 const User = require('../models/user')
 
 const router = express.Router()
+
+
+function generateToken(params = {}){
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    } )
+}
 
 router.post('/register', async(req, res) => {
     
     const {email} = req.body
     
     try {
+        
         if( await User.findOne({ email })){
             return res.status(400).send({error: 'User already exists'})
         }
 
         const user = await User.create(req.body)
-
         user.password = undefined
 
-        return res.send(user)
+        const token = generateToken({id: user.id})
+
+        return res.send({user, token})
+
     } catch (error) {
         res.status(400).send({error: error.stack})
     }
+
 })
 
 router.post('/authenticate', async (req, res) => {
@@ -35,8 +48,12 @@ router.post('/authenticate', async (req, res) => {
 
     if (!(await bcrypt.compare(password, user.password)))
         return res.status(400).send({error: 'Invalid password'})
+
+    user.password = undefined
+
+    const token = generateToken({ id: user.id })
        
-    res.send({user})
+    res.send({user, token})
 })
 
 module.exports = app => app.use('/auth', router)
